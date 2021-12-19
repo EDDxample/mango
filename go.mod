fcc628b75316e21ec0425e6b4f275a8a6d138e0a
EDD <EDDxample@gmail.com>
2021-12-19 17:45:24 +0100

    refactor fields

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module mango
 
-go 1.16
+go 1.17
--- src/network/protocol.go
+++ src/network/protocol.go
@@ -6,3 +6,19 @@ const (
 	STATUS
 	LOGIN
 )
+
+func Handshake(conn net.Conn) {
+	var handshake c2s.Handshake
+	handshake.ReadPacket(conn)
+	fmt.Println(handshake)
+
+	var request c2s.Request
+	request.ReadPacket(conn)
+	fmt.Println(request)
+
+	// S2C_response
+
+	var ping c2s.Ping
+	ping.ReadPacket(conn)
+	fmt.Println(ping)
+}
